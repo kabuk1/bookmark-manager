cfdd5f3f60857de3931d613338df261e28bb96c4
Leslie <leslie.liszcz@gmail.com>
2021-06-21 16:10:05 +0100

    added bookmarks page

--- app.rb
+++ app.rb
@@ -7,7 +7,11 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/' do
-    'Bookmark Manager'
+    erb :index
+  end
+
+  get '/bookmarks' do
+    erb :bookmarks
   end
 
   run! if app_file == $0
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -3,4 +3,11 @@ feature 'View bookmarks' do
     visit('/')
     expect(page).to have_content('Bookmark Manager')
   end
+
+  scenario 'visit bookmarks page to see list of bookmarks' do
+    visit('bookmarks')
+    expect(page).to have_content('https://dev.to/')
+    expect(page).to have_content('https://hackernoon.com/')
+    expect(page).to have_content('https://hashnode.com/')
+  end
 end
